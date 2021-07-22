1420942cdd8dbb93d75092f0400d523979471308
Leo <leo.vander.meulen@gmail.com>
2021-07-22 19:57:26 +0200

    First version

---
+++ RouterInfo.py
@@ -0,0 +1,210 @@
+import requests
+import base64
+import json
+
+appPayload = "nvram_get(time_zone)"  # Your payload
+appPayload = "wanlink()"  # Your payload
+appPayload = "memory_usage()"  # Your payload
+appPayload = "get_clientlist()"  # Your payload
+appPayload = "apps_info()"  # Your payload
+appPayload = "uptime()"  # Your payload
+appPayload = "qrate()"  # Your payload
+appPayload = "get_machine_name()"  # Your payload
+appPayload = "wanlink(ipaddr)"  # Your payload
+
+appPayload = "netdev(appobj)"  # Your payload
+
+
+class RouterInfo:
+
+    def __init__(self, ipaddress, username, password):
+        '''
+        Create the object and connect with the router
+        Parameters:
+        - ipaddress - IP Address of the router
+        - username - Root user name
+        - password - Password required to login
+        '''
+        self.ip = None
+        self.token = None
+        self.__authenticate(ipaddress, username, password)
+
+    def __authenticate(self, ipaddress, username, password):
+        '''
+        Authenticate the object with the router
+        Parameters:
+        - ipaddress - IP Address of the router
+        - username - Root user name
+        - password - Password required to login
+        '''
+        self.ip = ipaddress
+
+        string = "{}:{}".format(username, password)
+        logintoken = base64.b64encode(string.encode('ascii')).decode('ascii')
+
+        payload = "login_authorization={}".format(logintoken)
+        headers = {
+            'user-agent': "asusrouter-Android-DUTUtil-1.0.0.201"
+        }
+        try:
+            r = requests.post(url='http://{}/login.cgi'.format(self.ip), data=payload, headers=headers).json()
+        except:
+            return False
+        if "asus_token" in r:
+            self.token = r['asus_token']
+            return True
+        else:
+            return False
+
+    def __get(self, command):
+        '''
+        Private get method to execute a hook on the router and return the result
+        '''
+        if self.token:
+            payload = "hook={}".format(command)
+            headers = {
+                'user-agent': "asusrouter-Android-DUTUtil-1.0.0.201",
+                'cookie': 'asus_token={}'.format(self.token)
+            }
+            try:
+                r = requests.post(url='http://{}/appGet.cgi'.format(self.ip), data=payload, headers=headers)
+            except:
+                return None
+            return r.text
+        else:
+            return None
+
+    def get_uptime(self):
+        '''
+        Return uptime of the router
+        Format: {'since': 'Thu, 22 Jul 2021 14:32:38 +0200', 'uptime': '375001'}
+        '''
+        r = self.__get('uptime()')
+        since = r.partition(':')[2].partition('(')[0]
+        up = r.partition('(')[2].partition(' ')[0]
+        return json.loads('{' + '"since":"{}", "uptime":"{}"'.format(since, up) + '}')
+
+    def get_memory_usage(self):
+        '''
+        Return memory usage of the router
+        Format: {'mem_total': '262144', 'mem_free': '107320', 'mem_used': '154824'}
+        '''
+        s = self.__get('memory_usage()')
+        return json.loads('{' + s[17:])
+
+    def get_cpu_usage(self):
+        '''
+        Return CPU usage of the router
+        Format: {'cpu1_total': '38106047', 'cpu1_usage': '3395512',
+                 'cpu2_total': '38106008', 'cpu2_usage': '2384694'}
+        '''
+        s = self.__get('cpu_usage()')
+        return json.loads('{' + s[14:])
+
+    def get_clientlist(self):
+        '''
+        Obtain a list of all clients
+        Format: {"get_clientlist":{"AC:84:C6:6C:A7:C0":{"type": "2", "defaultType": "0", "name": "Archer_C1200",
+                                                        "nickName": "Router Forlindon", "ip": "192.168.2.175",
+                                                        "mac": "AC:84:C6:6C:A7:C0", "from": "networkmapd",
+                                                        "macRepeat": "1", "isGateway": "0", "isWebServer": "0",
+                                                        "isPrinter": "0", "isITunes": "0", "dpiType": "",
+                                                        "dpiDevice": "", "vendor": "TP-LINK", "isWL": "0",
+                                                        "isOnline": "1", "ssid": "", "isLogin": "0", "opMode": "0",
+                                                        "rssi": "0", "curTx": "", "curRx": "", "totalTx": "",
+                                                        "totalRx": "", "wlConnectTime": "", "ipMethod": "Manual",
+                                                        "ROG": "0", "group": "", "callback": "", "keeparp": "",
+                                                        "qosLevel": "", "wtfast": "0", "internetMode": "allow",
+                                                        "internetState": "1", "amesh_isReClient": "1",
+                                                        "amesh_papMac": "04:D4:C4:C4:AD:D0"
+                                  },
+                                  "maclist": ["AC:84:C6:6C:A7:C0"], "ClientAPILevel": "2" }}
+        '''
+        return json.loads(self.__get('get_clientlist()'))
+
+    # Total traffic in Mb/s
+    def get_traffic_total(self):
+        '''
+        Get total amount of traffic since last restart (Megabit format)
+        Format: {'sent': '15901.92873764038', 'recv': '10926.945571899414'}
+        '''
+        meas_2 = json.loads(self.__get('netdev(appobj)'))
+        tx = int(meas_2['netdev']['INTERNET_tx'], base=16) * 8 / 1024 / 1024 / 2
+        rx = int(meas_2['netdev']['INTERNET_rx'], base=16) * 8 / 1024 / 1024 / 2
+        return json.loads('{' + '"sent":"{}", "recv":"{}"'.format(tx, rx) + '}')
+
+    # Traffic in Mb/s . Megabit per second
+    # Note this method has a 2 second delay to calculate current throughput
+    def get_traffic(self):
+        '''
+        Get total and current amount of traffic since last restart (Megabit format)
+        Note there is a two second delay to determine current traffic
+        Format: {"speed": {"tx": 0.13004302978515625, "rx": 4.189826965332031},
+                 "total": {"sent": 15902.060073852539, "recv": 10931.135665893555}}
+        '''
+        meas_1 = self.__get('netdev(appobj)')
+        time.sleep(2)
+        meas_2 = self.__get('netdev(appobj)')
+        meas_1 = json.loads(meas_1)
+        meas_2 = json.loads(meas_2)
+        persec = {}
+        totaldata = {}
+        tx = int(meas_2['netdev']['INTERNET_tx'], base=16) * 8 / 1024 / 1024 / 2
+        totaldata['sent'] = tx
+        tx -= int(meas_1['netdev']['INTERNET_tx'], base=16) * 8 / 1024 / 1024 / 2
+        persec['tx'] = tx
+        rx = int(meas_2['netdev']['INTERNET_rx'], base=16) * 8 / 1024 / 1024 / 2
+        totaldata['recv'] = rx
+        rx -= int(meas_1['netdev']['INTERNET_rx'], base=16) * 8 / 1024 / 1024 / 2
+        persec['rx'] = rx
+        total = {}
+        total['speed'] = persec
+        total['total'] = totaldata
+        return (json.dumps(total))
+
+    def get_status_wan(self):
+        '''
+        Get the status of the WAN connection
+        Format: {"status": "1", "statusstr": "'Connected'", "type": "'dhcp'", "ipaddr": "'192.168.1.2'",
+                 "netmask": "'255.255.255.0'", "gateway": "'192.168.1.1'", "dns": "1.1.1.1'",
+                 "lease": "86400", "expires": "81967", "xtype": "''", "xipaddr": "'0.0.0.0'",
+                 "xnetmask": "'0.0.0.0'", "xgateway": "'0.0.0.0'", "xdns": "''", "xlease": "0",
+                 "xexpires": "0"}
+        '''
+        r = self.__get('wanlink()')
+        status = {}
+        for f in r.split('\n'):
+            if 'return' in f:
+                if 'wanlink_' in f:
+                    key = f.partition('(')[0].partition('_')[2]
+                    value = f.rpartition(' ')[-1][:-2]
+                    status[key] = value
+        return (json.dumps(status))
+
+    def get_settings(self):
+        '''
+        Get settings from the router
+        Format:{'time_zone': 'MEZ-1DST', 'time_zone_dst': '1', 'time_zone_x': 'MEZ-1DST,M3.2.0/2,M10.2.0/2',
+               'time_zone_dstoff': 'M3.2.0/2,M10.2.0/2', 'ntp_server0': 'pool.ntp.org', 'acs_dfs': '1',
+               'productid': 'RT-AC68U', 'apps_sq': '', 'lan_hwaddr': '04:D4:C4:C4:AD:D0',
+               'lan_ipaddr': '192.168.2.1', 'lan_proto': 'static', 'x_Setting': '1',
+               'label_mac': '04:D4:C4:C4:AD:D0', 'lan_netmask': '255.255.255.0', 'lan_gateway': '0.0.0.0',
+               'http_enable': '2', 'https_lanport': '8443', 'wl0_country_code': 'EU', 'wl1_country_code': 'EU'}
+        '''
+        settings = {}
+        for s in ['time_zone', 'time_zone_dst', 'time_zone_x', 'time_zone_dstoff', 'time_zone',
+                  'ntp_server0', 'acs_dfs', 'productid', 'apps_sq', 'lan_hwaddr', 'lan_ipaddr',
+                  'lan_proto', 'x_Setting', 'label_mac', 'lan_netmask', 'lan_gateway',
+                  'http_enable', 'https_lanport', 'wl0_country_code', 'wl1_country_code']:
+            r = self.__get("nvram_get(" + s + ")")
+            settings[s] = json.loads(r)[s]
+        return settings
+
+    def get_dhcp_list(self):
+        '''
+        Return a list of DHCP leases
+        Format: { "dhcpLeaseMacList":[["00:00:00:00:00:00", "name"], ...]
+}
+        '''
+        r = self.__get("dhcpLeaseMacList()")
+        return json.loads(r)
